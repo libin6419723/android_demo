e028473c54ded46684f1078ca3040608c4bbf6ed
unknown <6419723@qq.com>
2020-06-09 18:13:57 +0800

    test

--- app/src/main/java/com/kongzhong/android_demo/MainActivity.java
+++ app/src/main/java/com/kongzhong/android_demo/MainActivity.java
@@ -2,32 +2,65 @@ package com.kongzhong.android_demo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List <Demo> demoList = new ArrayList<>();
+    RecyclerView rv;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        demoList.add(new Demo("test", "testring", null));
+        demoList.add(new Demo("test2", "testring", null));
+        demoList.add(new Demo("test3", "testpaing", null));
+        demoList.add(new Demo("test", "testring", null));
+        demoList.add(new Demo("test2", "testring", null));
+        demoList.add(new Demo("test3", "testpaing", null));
+        demoList.add(new Demo("test", "testring", null));
+        demoList.add(new Demo("test2", "testring", null));
+        demoList.add(new Demo("test3", "testpaing", null));
+        demoList.add(new Demo("test", "testring", null));
+        demoList.add(new Demo("test2", "testring", null));
+        demoList.add(new Demo("test3", "testpaing", null));
+        rv = this.findViewById(R.id.rv_main);
+        DemoAdapter adapter  = new DemoAdapter(demoList);
+        LinearLayoutManager lm = new LinearLayoutManager(this);
+        rv.setLayoutManager(lm);
+        rv.setAdapter(adapter);
     }
 
     public class Demo {
-        public String name;
-        public String title;
-        public Activity activity;
+        public String mName;
+        public String mTitle;
+        public Class<Activity> mActivity;
+        public Demo(String name, String title, Class<Activity> activity) {
+            mName = name;
+            mTitle = title;
+            mActivity= activity;
+        }
     }
     public class DemoAdapter extends RecyclerView.Adapter<DemoAdapter.ViewHolder> {
 
         private List<Demo> mDemoList;
+        RecyclerView recyclerView;
 
         public DemoAdapter(List<Demo> demoList) {
             mDemoList = demoList;
@@ -36,12 +69,20 @@ public class MainActivity extends AppCompatActivity {
         @NonNull
         @Override
         public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.demo_item, parent, false);
+            ViewHolder holder = new ViewHolder(view);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            });
+            return holder;
         }
 
         @Override
         public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+            Demo demo = mDemoList.get(position);
+            holder.tvTitle.setText(demo.mTitle);
         }
 
         @Override
--- app/src/main/java/com/kongzhong/android_demo/ui/CircularRingView.java
+++ app/src/main/java/com/kongzhong/android_demo/ui/CircularRingView.java
@@ -1,6 +1,8 @@
 package com.kongzhong.android_demo.ui;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -13,33 +15,57 @@ public class CircularRingView extends View {
     public static final int b = Color.parseColor("#43E4F9");
     public static final int c = Color.parseColor("#FE9438");
     public static float d;
-    public Paint e;
-    public Paint f;
+    public Paint mPaint1;
+    public Paint mPaint2;
     public int g;
     public int h;
     public int i;
     public RectF j;
     public float k;
+    public Bitmap mIcon = null;
 
     public CircularRingView(Context context) {
         super(context);
         init();
     }
+
     public CircularRingView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
     }
 
+    public void setIcon() {
+    }
+
+    public void animator(float f2, boolean z) {
+        if (z) {
+            this.mPaint2.setColor(b);
+        } else {
+            this.mPaint2.setColor(c);
+        }
+        ValueAnimator ofFloat = ValueAnimator.ofFloat(new float[]{0.0f, (f2 / 100.0f) * 360.0f});
+        ofFloat.setDuration(2000);
+        ofFloat.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float unused = CircularRingView.this.k = ((Float) valueAnimator.getAnimatedValue()).floatValue();
+                CircularRingView.this.invalidate();
+            }
+        });
+        ofFloat.start();
+    }
+
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle((float) (this.g / 2), (float) (this.h / 2), ((float) this.i) - d, this.e);
+        canvas.drawCircle((float) (this.g / 2), (float) (this.h / 2), ((float) this.i) - d, this.mPaint1);
         if (this.j == null) {
             float f2 = d;
             this.j = new RectF(f2, f2, ((float) this.h) - f2, ((float) this.g) - f2);
         }
-        canvas.drawArc(this.j, -90.0f, this.k, false, this.f);
-
+        canvas.drawArc(this.j, -90.0f, this.k, false, this.mPaint2);
+        if (this.mIcon != null)
+            canvas.drawBitmap(this.mIcon, 0.0f, 0.0f, this.mPaint1);
     }
 
     @Override
@@ -51,26 +77,26 @@ public class CircularRingView extends View {
     }
 
 
-    public static int u2d(Context context, float dp) {
+    public static int dp2pixels(Context context, float dp) {
         return (int) ((dp * context.getResources().getDisplayMetrics().density) + 0.5f);
     }
 
     private void init() {
-        d = (float) this.u2d(getContext(), 4.0f);
-        this.e = new Paint();
-        this.e.setColor(a);
-        this.e.setAntiAlias(true);
-        this.e.setStyle(Paint.Style.STROKE);
-        this.e.setStrokeWidth(d);
-        this.e.setStrokeCap(Paint.Cap.ROUND);
-        this.e.setDither(true);
-        this.f = new Paint();
-        this.f.setStyle(Paint.Style.STROKE);
-        this.f.setStrokeWidth(d);
-        this.f.setColor(b);
-        this.f.setStrokeCap(Paint.Cap.ROUND);
-        this.f.setAntiAlias(true);
-        this.f.setDither(true);
+        d = (float) this.dp2pixels(getContext(), 4.0f);
+        this.mPaint1 = new Paint();
+        this.mPaint1.setColor(a);
+        this.mPaint1.setAntiAlias(true);
+        this.mPaint1.setStyle(Paint.Style.STROKE);
+        this.mPaint1.setStrokeWidth(d);
+        this.mPaint1.setStrokeCap(Paint.Cap.ROUND);
+        this.mPaint1.setDither(true);
+        this.mPaint2 = new Paint();
+        this.mPaint2.setStyle(Paint.Style.STROKE);
+        this.mPaint2.setStrokeWidth(d);
+        this.mPaint2.setColor(b);
+        this.mPaint2.setStrokeCap(Paint.Cap.ROUND);
+        this.mPaint2.setAntiAlias(true);
+        this.mPaint2.setDither(true);
     }
 
 }
